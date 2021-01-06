36e5b2fc6e80c266cce1bfec1f2737449fc7d514
dammmit777 <2775270442@qq.com>
2021-01-06 16:59:05 +0800

    deleted bin  build cmake_modules

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ssm/GRADUATION-PROJECT
-# Build directory: /home/ssm/GRADUATION-PROJECT/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(src)
-subdirs(test)
-subdirs(app)
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ssm/GRADUATION-PROJECT/src
-# Build directory: /home/ssm/GRADUATION-PROJECT/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
